f9b4e4767e07bbe86124e7fab9d32e01b6692ecf
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-03-18 22:53:02 +0900

    deleted simple methods

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -10,25 +10,8 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var outputLbl: UILabel!
+
     var btnSound: AVAudioPlayer!
-    
-    var currentOperation = Operation.Empty
-    var runningNumber = ""
-    var leftValStr = ""
-    var rightValStr = ""
-    var result = ""
-    
-    enum Operation: String {
-        case Divide = "/"
-        case Multiply = "*"
-        case Subtract = "-"
-        case Add = "+"
-        case Empty = "Empty"
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,40 +26,10 @@ class ViewController: UIViewController {
         } catch let err as NSError {
             print(err.debugDescription)
         }
-        
-        outputLbl.text = "0"
+
     }
     
-    @IBAction func numberPressed(sender: UIButton) {
-        playSound()
-        runningNumber += "\(sender.tag)"
-        outputLbl.text = runningNumber
-    }
-    
-    @IBAction func onDividePressed(sender: AnyObject) {
-        playSound()
-        processOperation(operation: .Divide)
-    }
-    
-    @IBAction func onMultiplyPressed(sender: AnyObject) {
-        playSound()
-        processOperation(operation: .Multiply)
-    }
-    
-    @IBAction func onSubtractPressed(sender: AnyObject) {
-        playSound()
-        processOperation(operation: .Subtract)
-    }
-    
-    @IBAction func onAddPressed(sender: AnyObject) {
-        playSound()
-        processOperation(operation: .Add)
-    }
-    
-    @IBAction func onEqualPressed(sender: AnyObject) {
-        playSound()
-        processOperation(operation: currentOperation)
-    }
+
     
     func playSound() {
         if btnSound.isPlaying {
@@ -85,35 +38,8 @@ class ViewController: UIViewController {
         btnSound.play()
     }
     
-    func processOperation(operation: Operation) {
-        if currentOperation != Operation.Empty {
-            
-            if runningNumber != "" {
-                rightValStr = runningNumber
-                runningNumber = ""
-                
-                if currentOperation == Operation.Multiply {
-                    result = "\(Double(leftValStr)!*Double(rightValStr)!)"
-                } else if  currentOperation == Operation.Divide {
-                    result = "\(Double(leftValStr)!/Double(rightValStr)!)"
-                } else if currentOperation == Operation.Subtract {
-                    result = "\(Double(leftValStr)!-Double(rightValStr)!)"
-                } else if currentOperation == Operation.Add {
-                    result = "\(Double(leftValStr)!+Double(rightValStr)!)"
-                }
-                
-                leftValStr = result
-                outputLbl.text = result
-            
-            }
-            
-            currentOperation = operation
-        
-        } else {
-            leftValStr = runningNumber
-            runningNumber = ""
-            currentOperation = operation
-        }
+    @IBAction func touchDigit(_ sender: Any) {
     }
+    
 
 }
